3a93b59088a5eb496049d6b404667370515d0899
himanshu272 <himanshu.shekharjha.272@gmail.com>
2019-05-14 09:49:00 +0530

    #2, add hidden tag

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, url_for, request
 from app import app
 from app.forms import Login
 @app.route('/')
@@ -8,7 +8,7 @@ def index():
 @app.route('/login',methods=['GET', 'POST'])
 def login():
 	form = Login()
-	if form.validate_on_submit():
-		flash('Login successful for username {}, remember= {}'.format(form.username.data,form.remember.data))
-		return redirect('/index')
+	if form.validate_on_submit() and request.method == 'POST':
+		flash('Login successful!')
+		return render_template("base.html",title="Home")
 	return  render_template("form.html",title='Login',form=form)
