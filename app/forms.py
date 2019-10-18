8d7915ff537c143d8504b8c6c24e9ff1efdd9d47
Aakash Dey <iamaakash007.hvm@gmail.com>
2019-10-18 10:10:35 +0530

    [DELETE] all flask files

--- app/forms.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import BooleanField, SubmitField, PasswordField, StringField
-from wtforms.validators import DataRequired
-
-class Login(FlaskForm):
-		username = StringField('USERNAME',validators=[DataRequired()])
-		password = PasswordField('PASSWORD',validators=[DataRequired()])
-		submit = SubmitField('Submit')
-		remember = BooleanField('Remember Me')
